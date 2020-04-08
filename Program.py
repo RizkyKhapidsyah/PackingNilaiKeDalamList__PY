02f2125382443685a0575e21ba694ab89221e22c
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-04-08 09:41:19 +0700

    Initial commitss

---
+++ Program.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+c = 30
+
+d = [None] * 3
+
+d[0] = a
+d[1] = b
+d[2] = c
+
+print(f'a = {a}, b = {b}, c = {c}')
+print(f'd = {d}')
---
+++ Program3.py
@@ -0,0 +1,6 @@
+a, b, c = 10, 20, 30
+d = [a, b, c]
+
+print(f'a = {a}, b = {b}, c = {c}')
+print(f'd = {d}') #List
+
